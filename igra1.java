b8f79aad150d7a678c65b45d2cc95c34fb3ad466
aleksei kozlov <aleksei22891@gmail.com>
2021-09-13 13:38:55 +0300

    sixth

--- igra1.java
+++ igra1.java
@@ -9,22 +9,41 @@ public class igra1 {
         Random random = new Random();
         int number;
         int player;
-        number = random.nextInt(11)+1;
+        number = random.nextInt(10)+1;
+        // игра с циклом for
         for (int i = 0; i <5;i++){
             System.out.print("Your number is ");
             player = chislo.nextInt();
-            if (player > number){
-                System.out.println("This number is greater than the specified number");
-            }else if (player < number){
-                System.out.println("This number is less than the specified number");
-            }
-            else{
+            if ((player > number-3 && player < number+3) && player != number){
+                System.out.println("This is hot");
+            }else if ((player >=1 && player <= 10) && player != number){
+                System.out.println("This is cold");
+            }else if (player ==  number){
                 System.out.println("This number true");
                 break;
             }
             if (i == 4 && player !=  number){
                 System.out.println("You lose, true number is "+number);
             }
+        }
+        // игра с циклом while
+        int j =0;
+        number = random.nextInt(10)+1;
+        while(j<4 ){
+            System.out.print("Your number is ");
+            player = chislo.nextInt();
+            if ((player > number-3 && player < number+3) && player != number){
+                System.out.println("This is hot");
+            }else if ((player >=1 && player <= 10) && player != number){
+                System.out.println("This is cold");
+            }else if (player ==  number){
+                System.out.println("This number true");
+                break;
+            }
+            if (j == 4 && player !=  number){
+                System.out.println("You lose, true number is "+number);
+            }
+            j++;
         }
     }
 }
